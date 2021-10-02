5ec988c14fc9f5fad86c47ae3344c3088f9e83ac
Rudys Natanael Acosta <natanael926@gmail.com>
2021-10-02 10:34:04 -0400

    Refactory style of timelinecontent

--- frontend/src/components/patient/TimeLineContent.js
+++ frontend/src/components/patient/TimeLineContent.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import { deleteBackground } from '../../actions/patient';
 
 import PropTypes from "prop-types";
-import {FaEdit, FaWindowClose} from 'react-icons/fa'
+import {FaEdit, FaWindowClose, FaCalendarAlt} from 'react-icons/fa'
 
 
 import ModalFormAction from './ModalFormAction'
@@ -39,14 +39,21 @@ export class TimeLineContent extends Component {
                 </div>
                 <div className={`order-1 rounded-lg shadow-xl w-5/12 px-6 py-4 ${backgroundAndColor}`}>
                     <div className="flex flex-row justify-between">
-                        <h3 className="mb-3 font-bold  text-xl capitalize">{this.props.data.title}</h3>
+                        <div className="flex flex-col space-y-0 mb-2">
+                            <h3 className="font-bold  text-xl capitalize">{this.props.data.title}</h3>
+                            <div className="flex flex-row space-x-1 text-gray-500 text-sm">
+                                <FaCalendarAlt />
+                                <span>{this.props.data.date}</span>
+                            </div>
+                        </div>
+                        
                         <div className="flex flex-row space-x-2">
                             <FaEdit onClick={() => this.props.onEdit()}  className="text-blue-500 hover:text-blue-700 cursor-pointer"/>
                             <FaWindowClose onClick={() => this.onDeleteObj(this.props.data.id)} className="text-red-500 hover:text-red-700 cursor-pointer"/>
                         </div>
                     </div>
                 
-                    <div className="text-sm leading-snug tracking-wide text-opacity-100">
+                    <div className="text-sm leading-snug tracking-wide text-opacity-100 bg-gray-100 rounded-md p-2">
                         {this.props.data.content}
                     </div>
                 </div>
