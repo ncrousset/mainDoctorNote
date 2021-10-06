d156e69d08f0d827211a3048dfb750bed92468f9
Rudys Natanael Acosta <natanael926@gmail.com>
2021-10-06 12:41:20 -0400

    Return config setting

--- config/settings.py
+++ config/settings.py
@@ -3,9 +3,8 @@ from pathlib import Path
 import environ
 import os
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env(DEBUG=(bool, False))
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
