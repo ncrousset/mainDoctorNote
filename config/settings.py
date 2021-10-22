6389c817babbc31f4ab7808bbc3d50686658a641
Rudys Natanael Acosta <natanael926@gmail.com>
2021-10-21 20:17:59 -0400

    Add url base

--- config/settings.py
+++ config/settings.py
@@ -8,10 +8,12 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_URL = 'http://127.0.0.1:8000/'
+
 
 env = environ.Env(DEBUG=(bool, False))
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+BASE_URL = env("URL")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
